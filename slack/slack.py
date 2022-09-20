d248f44a01b101629c39c54aae44cea46bbe5077
rilmayer <tomohiro.furusawa.629@gmail.com>
2022-09-21 08:49:47 +0900

    Remove post text header

--- slack/slack.py
+++ slack/slack.py
@@ -1,15 +1,13 @@
 import random
 import slackweb
 
-HEADER_TEXT = "*Today's Pickup News*"
-
 def make_post(contents):
     if len(contents) == 0:
         return "今日のニュースはありません。"
     
     picked_content = random.sample(contents, 1)
     title, url = picked_content[0]["title"], picked_content[0]["url"]
-    return f"{HEADER_TEXT}\n{title}\n{url}"
+    return f"{title}\n{url}"
 
 def post_text_to_slack(text, slack_url):
     slack = slackweb.Slack(url=slack_url)
